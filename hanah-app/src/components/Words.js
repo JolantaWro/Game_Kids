7c651c1cfa8b5ccf869411d0bc5b9341d2f4335a
JolantaWro <jolanta.woronowska@gmail.com>
2023-03-23 11:00:54 +0100

    add game words

--- hanah-app/src/components/Words.js
+++ hanah-app/src/components/Words.js
@@ -1,21 +1,30 @@
-import {React, useState} from "react";
+import {React, useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 
 const Words = () => {
     const firstWord = ["mama", "tata", "Hania", "Zosia", "Babcia"]
     const [words, setWords] = useState(firstWord)
+    const [isRuning, setIsRuning] = useState(false)
+    const [index, setIndex] = useState(0);
 
+    const setNumberIndex = () => setIndex(i => i + 1);
+
+    useEffect(() => {
+        let intervalWords;
+        if(isRuning) {
+            intervalWords = setInterval(setNumberIndex, 3000);
+        }
+        return () => clearInterval(intervalWords);
+    }, [isRuning])
 
 
 
     return(
         <div>
             <Link to='/' style={{ textDecoration: 'none'}}>Strona startowa</Link>
-
-            {
-                words.map((element) => <h1 style={{color:"red"}}>{element}</h1>)
-            }
-
+            <h1 className="wordsView">{words[index % words.length]}</h1>
+            <button onClick={()=>setIsRuning(true)} className="answer">Start</button>
+            <button onClick={()=>setIsRuning(false)} className="answer">Stop</button>
         </div>
     )
 }
